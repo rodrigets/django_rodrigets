e62af039e59b82210be18a93618b7e1c5fd4116b
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2018-06-06 15:32:50 -0300

    incluindo metodo update

--- project_django/clientes/urls.py
+++ project_django/clientes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import persons_list
 from .views import person_new
+from .views import person_update
 
 urlpatterns = [
     path('list/', persons_list, name="person_list"),
     path('new/', person_new, name="person_new"),
+    path('update/<int:id>/', person_update, name="person_update"),
 ]
